0c58e06a701aee3481048f26eb9d1f64ef04fa02
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2022-12-17 21:03:24 +0200

    upadate

--- src/hooks/hook.http.js
+++ src/hooks/hook.http.js
@@ -1,7 +1,7 @@
 export const useHttp = () => {
-    const request = async (url, headers = {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'https://movies-db-alpha.vercel.app/'}, method = "GET", body = null) => {
+    const request = async (url, headers = {'Content-Type': 'application/json'}, method = "GET", body = null) => {
         try {
-            const response = await fetch(url, {method, body, headers})
+            const response = await fetch(url, {method, body, headers, })
 
             if (!response.ok) {
                 throw new Error(`Could not fetch ${url}, status: ${response.status}`);
--- src/proxy.js
+++
@@ -1,6 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = app => {
-    app.use('/api', createProxyMiddleware({ target: 'https://videocdn.tv/', changeOrigin: true }));
-    app.use('/api', createProxyMiddleware({ target: 'https://api.themoviedb.org/', changeOrigin: true }));
-}
