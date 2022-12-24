98c414d951c6ad1974145a58faebc1fc1092b589
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2022-12-24 22:31:58 +0200

    added reset password

---
+++ src/components/auth/resetPassword/ResetPassword.js
@@ -0,0 +1,52 @@
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+import { useNavigate } from 'react-router-dom';
+
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
+
+import '../form/form.scss';
+
+function ResetPassword() {
+
+    const navigate = useNavigate()
+
+    const handleSubmit = async email => {
+        try {
+            const auth = getAuth()
+            await sendPasswordResetEmail(auth, email)
+            navigate('/login')
+            toast.success('Сообщение было отправлено!')
+        } catch(err) {
+            toast.error('Не получилось отправить сброс пароля!')
+        }
+    }
+
+  return (
+    <section className='auth'>
+        <div className='container'>
+            <Formik initialValues={{email: ''}}
+                validationSchema = {Yup.object({
+                    email: Yup.string()
+                            .email('Неправильный email адрес')
+                            .required('Email обязателен для заполнения!'),
+                })}
+                onSubmit = {values => {
+                    JSON.stringify(values, null, 2)
+                    handleSubmit(values.email)
+                }}
+            >
+                <Form>
+                    <h1>Введите почту</h1>
+                    <Field name="email" placeholder='Почта' />
+                    <ErrorMessage name="email" className='error' component="div" />
+                    <button>Сбросить пароль</button>
+                </Form>
+            </Formik>
+        </div>
+    </section>
+  )
+}
+
+export default ResetPassword
