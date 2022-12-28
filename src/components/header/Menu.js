5073f7710ce14920760a144fe78521cd046e5d39
Maxim Chervonoshtan <chervonoshtan02@gmail.com>
2022-12-28 19:33:11 +0200

    fix adaptive menu

--- src/components/header/Menu.js
+++ src/components/header/Menu.js
@@ -1,32 +1,25 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { toggleMobileMenu } from '../../reduxSlice/main'
 
 function Menu() {
+    const dispatch = useDispatch()
     const { showMobileMenu } = useSelector(state => state.main)
+
+    const handleHideMenu = e => {
+        if (e.target.classList.contains('close_menu') || !e.target.closest('.menu_list')) {
+            dispatch(toggleMobileMenu(true))
+        }
+    }
 
   return (
     <>
-        <nav className='nav_desktop'>
-            <ul>
-                <li className='has_children'><span><Link to="category/movie/popular/page/1">Фильмы</Link></span>
-                    <ul className='sub_menu'>
-                        <li><Link to="category/movie/popular/page/1">Популярные</Link></li>
-                        <li><Link to="category/movie/top_rated/page/1">В топе</Link></li>
-                        <li><Link to="category/movie/upcoming/page/1">Скоро выйдут</Link></li>
-                    </ul>
-                </li>
-                <li className='has_children'><span><Link to="category/tv/popular/page/1">Сериалы</Link></span>
-                    <ul className='sub_menu'>
-                        <li><Link to="category/tv/popular/page/1">Популярные</Link></li>
-                        <li><Link to="category/tv/top_rated/page/1">В топе</Link></li>
-                        <li><Link to="category/tv/airing_today/page/1">Новинки</Link></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-
-        <nav className={`nav_mobile ${showMobileMenu ? 'active' : ''} `}>
-            <ul>
+        <nav className={showMobileMenu ? 'active' : ''} onClick={e => handleHideMenu(e)}>
+            <div className='close_menu'>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className='menu_list'>
                 <li className='has_children'><span><Link to="category/movie/popular/page/1">Фильмы</Link></span>
                     <ul className='sub_menu'>
                         <li><Link to="category/movie/popular/page/1">Популярные</Link></li>
